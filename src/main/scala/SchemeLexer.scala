3245c479435779312e778a94dc852e8b2e3fba18
Nole <universe_fly@icloud.com>
2021-05-27 14:42:28 +0800

    Rename Lexer/Parser to SchemeLexer/SchemeParser

--- src/main/scala/SchemeLexer.scala
+++ src/main/scala/SchemeLexer.scala
@@ -11,13 +11,13 @@ enum Token extends Positional:
   case HashLParen /* #( */
   case CommaAtSign /* ,@ */
 
-object Lexer extends JavaTokenParsers:
+object SchemeLexer extends JavaTokenParsers:
   // Don't skip whitespace by default, otherwise each SUBPARSER will skip
   // whitespaces before they're run. These subparsers are just components
   // of main parsers which we really care and which need skipping whitespaces.
   override def skipWhitespace = false
 
-  def token: Parser[Token] =
+  def token: Parser[Token] = positioned {
     identifier | boolean | number | character | string
       | "(" ^^ (_ => Token.LParen)
       | ")" ^^ (_ => Token.RParen)
@@ -27,6 +27,7 @@ object Lexer extends JavaTokenParsers:
       | withDelimiterAfter { "." ^^ (_ => Token.Dot) }
       | "(#" ^^ (_ => Token.HashLParen)
       | ",@" ^^ (_ => Token.CommaAtSign)
+  }
 
   /// Returning `Unit`` means we don't care what they return
   def delimiter: Parser[Unit] = """[ \(\)";]|\Z""".r ^^^ ()
@@ -39,13 +40,14 @@ object Lexer extends JavaTokenParsers:
   def atmosphere: Parser[Unit] = (whiteSpace | comment) ^^^ ()
   def intertokenSpace: Parser[Unit] = this.rep(atmosphere) ^^^ ()
 
-  def identifier: Parser[Token] =
+  def identifier: Parser[Token] = positioned {
     withDelimiterAfter {
       (initial ~ this.rep(subsequent)) ^^ { case initial ~ subsequents =>
         Token.ID(initial + subsequents.mkString)
       }
         | peculiarIdentifier ^^ { id => Token.ID(id) }
     }
+  }
 
   def initial: Parser[String] = letter | specialInitial
   def letter: Parser[String] = "[a-zA-Z]".r
@@ -64,27 +66,33 @@ object Lexer extends JavaTokenParsers:
     "(quote)|(lambda)|(if)|(set!)|(begin)|(cond)|(and)|(or)|(case)|(let)|(let*)|(letrec)|(do)|(delay)|(quasiquote)".r ^^^ ()
   def variable: Parser[Unit] = this.not(syntacticKeyword) ~> identifier ^^^ ()
 
-  def boolean: Parser[Token] =
+  def boolean: Parser[Token] = positioned {
     "#t" ^^ (_ => Token.Bool(true))
       | "#f" ^^ (_ => Token.Bool(false))
+  }
 
-  def character: Parser[Token] = withDelimiterAfter {
-    """#\""" ~> characterName ^^ (c => Token.Char(c))
-      | """#\\.""".r ^^ (s => Token.Char(s.charAt(2)))
+  def character: Parser[Token] = positioned {
+    withDelimiterAfter {
+      """#\""" ~> characterName ^^ (c => Token.Char(c))
+        | """#\\.""".r ^^ (s => Token.Char(s.charAt(2)))
+    }
   }
 
   def characterName: Parser[Char] =
-    "space" ^^ (_ => ' ')
-      | "newline" ^^ (_ => '\n')
+    "space" ^^ (_ => ' ') | "newline" ^^ (_ => '\n')
 
-  def string: Parser[Token] = "\"" ~> this.rep(stringElement) <~ "\"" ^^ {
-    strings => Token.String(strings.mkString)
+  def string: Parser[Token] = positioned {
+    "\"" ~> this.rep(stringElement) <~ "\"" ^^ { strings =>
+      Token.String(strings.mkString)
+    }
   }
 
   def stringElement: Parser[String] = """[^"\\]|(\\")|(\\\\f)""".r
 
-  def number = withDelimiterAfter {
-    """[+-]?(\d+(\.\d*)?|\d*\.\d+)""".r ^^ (d => Token.Num(BigDecimal(d)))
+  def number = positioned {
+    withDelimiterAfter {
+      """[+-]?(\d+(\.\d*)?|\d*\.\d+)""".r ^^ (d => Token.Num(BigDecimal(d)))
+    }
   }
 
   def tokens: Parser[List[Token]] =
@@ -92,4 +100,4 @@ object Lexer extends JavaTokenParsers:
 
   def apply(code: String) = parseAll(tokens, code) match
     case Success(result, _) => result
-    case failure            => scala.sys.error(failure.toString)
+    case failure    => scala.sys.error(failure.toString)
---
+++ src/main/scala/SchemeParser.scala
@@ -0,0 +1,103 @@
+import scala.util.parsing.combinator._
+import scala.util.parsing.input.{Reader, Position, NoPosition}
+
+object SchemeParser extends Parsers {
+  override type Elem = Token
+
+  class TokenReader(tokens: List[Token]) extends Reader[Token] {
+    override def first = tokens.head
+    override def atEnd = tokens.isEmpty
+    override def pos = tokens.headOption.map(_.pos).getOrElse(NoPosition)
+    override def rest = TokenReader(tokens.tail)
+  }
+
+  def program: Parser[Expr] = positioned {
+    phrase(expression)
+  }
+
+  def expression = positioned {
+    variable | literal | procedureCall | lambdaExpression
+  }
+
+  def literal = positioned {
+    selfEvaluating
+  }
+
+  def selfEvaluating: Parser[Expr] = positioned {
+    boolean | number | character | string
+  }
+
+  def procedureCall = positioned {
+    Token.LParen ~> operator ~ this.rep(operand) <~ Token.RParen ^^ {
+      case operator ~ operands => Expr.ProcedureCall(operator, operands)
+    }
+  }
+
+  def operator: Parser[Expr] = positioned {
+    expression
+  }
+
+  def operand: Parser[Expr] = positioned {
+    expression
+  }
+
+  def lambdaExpression = positioned {
+    Token.LParen ~> formals ~ body ^^ { case formals ~ body =>
+      Expr.LambdaExpr(formals, Nil, body)
+    }
+  }
+
+  def formals = {
+    Token.LParen ~> variable.* <~ Token.RParen
+      | variable ^^ (v => List(v))
+      | Token.LParen ~> variable.+ <~ Token.Dot
+  }
+
+  def body = sequence
+
+  def sequence: Parser[List[Expr]] = command.* ~ expression ^^ {
+    case commands ~ expression => commands :+ expression
+  }
+
+  def command: Parser[Expr] = positioned {
+    expression
+  }
+
+  def variable: Parser[Expr.Var] = positioned {
+    accept("Variable", { case Token.ID(name) => Expr.Var(name) })
+  }
+
+  def lambda: Parser[Unit] = accept(
+    "The 'lambda' keyword",
+    { case Token.ID(name) if name == "lambda" => () }
+  )
+
+  def boolean: Parser[Expr] = positioned {
+    accept("Boolean", { case Token.Bool(v) => Expr.Lit(Literal.Bool(v)) })
+  }
+
+  def number: Parser[Expr] = positioned {
+    accept("Number", { case Token.Num(v) => Expr.Lit(Literal.Num(v)) })
+  }
+
+  def character: Parser[Expr] = positioned {
+    accept("Character", { case Token.Char(c) => Expr.Lit(Literal.Char(c)) })
+  }
+
+  def string: Parser[Expr] = positioned {
+    accept("String", { case Token.String(s) => Expr.Lit(Literal.String(s)) })
+  }
+
+  def apply(tokens: List[Token]): Expr = {
+    val reader = TokenReader(tokens)
+    program(reader) match {
+      case Success(result, _) => result
+      case failure            => scala.sys.error(failure.toString)
+    }
+  }
+
+  def apply(code: String): Expr =
+    val tokens = SchemeLexer(code)
+    val AST = apply(tokens)
+    AST
+}
