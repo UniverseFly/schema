e127bdede3a0ca16daf0915aaadea96e08b46cff
Nole <universe_fly@icloud.com>
2021-06-20 20:26:30 +0800

    Handle exception in REPL

--- src/main/scala/schema/REPL.scala
+++ src/main/scala/schema/REPL.scala
@@ -5,13 +5,29 @@ import Console.flush
 import io.StdIn.readLine
 
 object REPL {
+  val shouldExit = (s: String) => s == null || s == "exit" || s == "quit"
+  val shouldSkip = (s: String) => s == ""
+
   def apply(prompt: String = "schema> ") = {
     val env = Builtins.stdEnv
-    while (true) {
-      print(prompt)
-      flush()
-      val expr = Parser(readLine())
-      println(eval(expr, env))
+    var done = false
+    println("Welcome to schema REPL, created by Yuxiang in 2021!")
+    while (!done) {
+      try {
+        print(prompt)
+        flush()
+        val input = readLine()
+        if shouldExit(input) then {
+          println(f"Bye!")
+          done = true
+        } else if shouldSkip(input) then ()
+        else {
+          val expr = Parser(input)
+          println(eval(expr, env))
+        }
+      } catch {
+        case e => System.err.println(e)
+      }
     }
   }
 }
